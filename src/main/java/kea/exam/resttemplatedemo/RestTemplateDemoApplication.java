1716dbc7a04bf4d943b6759a5e7f62c277ee33ad
Forkeh <forkeh@gmail.com>
2024-04-29 09:12:44 +0200

    initial commit

--- src/main/java/kea/exam/resttemplatedemo/RestTemplateDemoApplication.java
+++ src/main/java/kea/exam/resttemplatedemo/RestTemplateDemoApplication.java
@@ -6,8 +6,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class RestTemplateDemoApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(RestTemplateDemoApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(RestTemplateDemoApplication.class, args);
+    }
 
 }
